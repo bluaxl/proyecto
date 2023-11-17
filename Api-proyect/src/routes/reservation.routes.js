b9ba45ef5f31c7088f7a5101bb4f1bd62f39be06
bluaxl <bluaxl102@gmail.com>
2023-11-17 18:42:23 -0500

    .

--- Api-proyect/src/routes/reservation.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { tipoReserva } from "../controller/reservationType.controller.js";
-
-const router = Router();
-router.post('/documents', tipoReserva)
-
-export default router
