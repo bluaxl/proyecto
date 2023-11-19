9287b9606fecd331db0021e5a02605cf8952bc63
Gilaufen <131225939+Gilaufen@users.noreply.github.com>
2023-11-18 23:53:54 -0500

    NEW: data

--- arquideco.sql
+++ arquideco.sql
@@ -104,11 +104,16 @@ values ("Asesoria legal"),
 /*la constraseña está en SHA-256*/
 INSERT INTO Usuario (nombre, apellido, correoElectronico, numIdentificacion,telefono, tipoIdentificacion, contraseña, idRolFK, estado)
 VALUES ('Juan', 'Pérez', 'loreingp@hotmail.com', 123456789, 3103480318, 'Cedula de ciudadania', 'ac1be349ec1a3cf8bf3d96d11b62c7ec015355b450093a33e79d3cc8f0b9232e', 1, true),
-('María', 'Gómez', 'loreingp@hotmail.com', 987654321,3103480318, 'Pasaporte', '33e7183996951c46189f74338104f97e2b05f3d1a1d8d8e11de36c24f431a1d5', 2, 1),
+('María', 'Gómez', 'loreingp@hotmail.com', 987654321,3103480318, 'Pasaporte', '33e7183996951c46189f74338104f97e2b05f3d1a1d8d8e11de36c24f431a1d5', 2, false),
 ('Carlos', 'López', 'loreingp@hotmail.com', 555555555,3103480318, 'Cedula de ciudadania', '0c34ab7c7c92917a9d6cd7392185cd189ae1e6f910d65d679b8985248ac3d35f', 3, true),
-('Laura', 'Martínez', 'loreingp@hotmail.com', 111111111,3103480318, 'Pasaporte', '343eaeac32d1259f7b0d3bfdef3fb9ea65129b6844f034ff184b25b3f130664', 1, 1),
-('Ana', 'Rodríguez', 'loreingp@hotmail.com', 999999999,3103480318, 'Cedula de ciudadania', '13e83664ea35db30095e98492a7b0e14bbcbfadd23d7b4cb3bf6e1105b3ff54', 3, true);
-
+('Laura', 'Martínez', 'loreingp@hotmail.com', 111111111,3103480318, 'Pasaporte', '343eaeac32d1259f7b0d3bfdef3fb9ea65129b6844f034ff184b25b3f130664', 1, false),
+('Ana', 'Rodríguez', 'loreingp@hotmail.com', 999999999,3103480318, 'Cedula de ciudadania', '13e83664ea35db30095e98492a7b0e14bbcbfadd23d7b4cb3bf6e1105b3ff54', 3, true),
+ ('Luis', 'Hernández', 'loreingp@hotmail.com', 777777777, 3103480318, 'Cedula de ciudadania', '4a2e3bb4f24a36052933bf7aee2a5a1e17ef510d3d32e02bf2d195f60b7f66db', 1, true),
+('Diana', 'Ramírez', 'loreingp@hotmail.com', 888888888, 3103480318, 'Pasaporte', '9d55c028a2b0e5718ce7f36de99ea7684dbef1d751a81bb3c6e63519b961731f', 1, true),
+('Roberto', 'García', 'loreingp@hotmail.com', 666666666, 3103480318, 'Cedula de ciudadania', '6b82e4c86487dd01d13e15ad1d52f96311c2764a35d086b8d76e1e5b6e4e6410', 1, true),
+('Sofía', 'Fernández', 'loreingp@hotmail.com', 444444444, 3103480318, 'Pasaporte', '28d75893b24ac7c112b6b32a0807633c2a1a1d3e387a9d4ed94eb64c9ec41b2d', 1, true),
+('Eduardo', 'Gómez', 'loreingp@hotmail.com', 222222222, 3103480318, 'Cedula de ciudadania', '8e235bb9521bb70254c6c23fc1e0bf6841e9a193508b1b856c2f7ebcb5cd80de', 1, true);
+    
 insert into detalletiporeserva(datoDetTipoReserva, idTipoReservaFK)
 values ("Descripcion",1),
 ("tipo avaluo",2),
@@ -122,19 +127,45 @@ INSERT INTO Solicitud (FechaSolicitud, HoraSolicitud, estado)
 VALUES ('2023-09-07', '14:30:00', true),
 ('2023-09-07', '15:00:00', true),
 ('2023-09-07', '16:00:00' , false),
-('2023-09-07', '17:00:00', false),
-('2023-09-07', '18:00:00', false);
+('2023-09-07', '17:00:00', true),
+('2023-09-07', '18:00:00', false),
+('2023-09-07', '18:00:00', true),
+('2023-09-07', '18:00:00', false),
+('2023-09-07', '18:00:00', true),
+('2023-09-07', '18:00:00', true),
+('2023-09-07', '18:00:00', true),
+('2023-09-07', '18:00:00', true),
+('2023-09-07', '18:00:00', true),
+('2023-09-07', '18:00:00', true);
 
 insert into solicitudusuario(idSolictudFK, idClienteFK, idAsesorFK)
 values (1, 1, 3),
-(4, 4, 3);
+(4, 4, 3),
+(5, 4, 3),
+(6, 4, 3),
+(7, 4, 3),
+(8, 4, 3),
+(9, 4, 3),
+(10, 4, 3),
+(11, 4, 3),
+(12, 4, 3),
+(13, 4, 3)
+;
 
 insert into datossolicitud(datoSolicitud,idDetalleReservaFK,idSolicitudFK)
 values("solicitar acompañamiento en el proceso de prestamo de $5'000.000 de pesos",1,1),
+("solicitar acompañamiento en el proceso de prestamo de $5'000.000 de pesos",1,5),
+("solicitar acompañamiento en el proceso de prestamo de $5'000.000 de pesos",1,6),
+("solicitar acompañamiento en el proceso de prestamo de $5'000.000 de pesos",1,7),
+("solicitar acompañamiento en el proceso de prestamo de $5'000.000 de pesos",1,8),
+("solicitar acompañamiento en el proceso de prestamo de $5'000.000 de pesos",1,9),
+("solicitar acompañamiento en el proceso de prestamo de $5'000.000 de pesos",1,10),
+("solicitar acompañamiento en el proceso de prestamo de $5'000.000 de pesos",1,11),
+("solicitar acompañamiento en el proceso de prestamo de $5'000.000 de pesos",1,12),
+("solicitar acompañamiento en el proceso de prestamo de $5'000.000 de pesos",1,13),
 ("Para venta",2,4),
 ("image1.jpg",3,4),
 ("image2.jpg",4,4);
-
 
 select * from inmueble;
 select * from rol;
@@ -184,10 +215,10 @@ DELIMITER ;
 
 /*Insertar datos en la tabla solicitudUsuario*/
 DELIMITER //
-CREATE PROCEDURE sp_insert_soliUsuario (p_idSolicitud INT, p_idAsesor INT)
+CREATE PROCEDURE sp_insert_soliUsuario (p_idSolicitud INT, p_idCliente INT, p_idAsesor INT)
 BEGIN
-	insert into solicitudusuario(idSolicitudFK, idClienteFK, idAsesorFK)
-    values(p_idSolicitud, 1, p_idAsesor);
+	insert into solicitudusuario(idSolictudFK, idClienteFK, idAsesorFK)
+    values(p_idSolicitud, p_idCliente, p_idAsesor);
 END;
 //
 DELIMITER ;
@@ -324,14 +355,7 @@ SELECT distinct u.idUsuario, u.nombre, u.apellido,u.telefono , u.correoElectroni
 
 select * from view_usuario;
 
-DELIMITER //
-CREATE PROCEDURE sp_insert_soliUsuario (p_idSolicitud INT, p_idCliente INT, p_idAsesor INT)
-BEGIN
-	insert into solicitudusuario(idSolictudFK, idClienteFK, idAsesorFK)
-    values(p_idSolicitud, p_idCliente, p_idAsesor);
-END;
-//
-DELIMITER ;
+
 
 /*Estadística de cantidad de reservas y solicitudes realizadas*/
 DELIMITER //
@@ -344,5 +368,51 @@ END;
 //
 DELIMITER ;
 
+/*Estadística de cantidad de reservas y solicitudes realizadas por rango de fechas*/
+
+DELIMITER //
+CREATE PROCEDURE sp_contar_reservas_con_fecha(IN p_estado INT, IN p_fecha_inicio DATE, IN p_fecha_fin DATE)
+BEGIN
+    SELECT COUNT(DISTINCT idSolicitud) AS cantidad
+    FROM view_sol_reserva
+    WHERE estado = p_estado
+        AND fechaSolicitud BETWEEN p_fecha_inicio AND p_fecha_fin;
+END;
+//
+DELIMITER ;
+
+/*Estadística de cantidad de usuarios activos*/
+DELIMITER //
+CREATE PROCEDURE sp_contar_usuarios()
+BEGIN
+    SELECT COUNT(DISTINCT idUsuario) AS cantidad
+    FROM view_usuario
+    WHERE estado = 1;
+END;
+//
+DELIMITER ;
+
+/*estadistica mayor cantidad de reservas por asesor*/
+DELIMITER //
+CREATE PROCEDURE sp_asesor_mas_reservas()
+BEGIN
+    SELECT idAsesor, COUNT(DISTINCT idSolicitud) AS cantidadReservas
+    FROM view_sol_reserva
+    WHERE estado = 0
+    GROUP BY idAsesor
+    ORDER BY cantidadReservas DESC
+    LIMIT 1;
+END;
+//
+DELIMITER ;
 
 
+DELIMITER //
+CREATE PROCEDURE sp_contar_inmuebles()
+BEGIN
+    SELECT COUNT(DISTINCT idInmueble) AS cantidad
+    FROM inmueble
+    WHERE clasificacion = 1;
+END;
+//
+DELIMITER ;
