ea620418ec6abbcb105f2171958edab6315f18e6
Gilaufen <131225939+Gilaufen@users.noreply.github.com>
2023-11-26 14:16:20 -0500

    FIX: datetime button

--- proyecto-Front-end/src/components/client/home/StateDetail.js
+++ proyecto-Front-end/src/components/client/home/StateDetail.js
@@ -24,31 +24,33 @@ const Modal = ({ isOpen, onClose, children }) => {
 function DateTimeButton() {
     // Función para manejar el click en el botón de fecha
     const handlerOnclick = () => {
-        let fecha = new Date();
-        let anio = fecha.getFullYear();
-        let dia = fecha.getDate();
-        let _mes = fecha.getMonth();
-        _mes = _mes + 1;
-        if (_mes < 10) {
-            var mes = "0" + _mes;
-        } else {
-            var mes = _mes.toString;
-        }
-        let fecha_minimo = anio + '-' + mes + '-' + dia;
-
-    }
-
+      let fecha = new Date();
+      let anio = fecha.getFullYear();
+      let dia = fecha.getDate();
+      let _mes = fecha.getMonth();
+      _mes = _mes + 1;
+      if (_mes < 10) {
+        var mes = "0" + _mes;
+      } else {
+        var mes = _mes.toString();
+      }
+      let fecha_minimo = anio + '-' + mes + '-' + dia;
+      console.log(fecha_minimo);
+      return fecha_minimo
+      
+    };
+  
     return (
-        <div className="datetime-box__catalogue">
-            {/* Título de selección de fecha y hora */}
-            <h4>Seleccione la fecha y hora</h4>
-            {/* Input de selección de fecha */}
-            <input type="date" id="fechaReserva" className="fechaReserva" onClick={handlerOnclick}></input>
-            {/* Input de selección de hora */}
-            <input type="time" className="horaReserva" id="horaReserva" min="07:00" max="17:00" step="1800"></input>
-        </div>
-    )
-}
+      <div className="datetime-box__catalogue">
+        {/* Título de selección de fecha y hora */}
+        <h4>Seleccione la fecha y hora</h4>
+        {/* Input de selección de fecha */}
+        <input type="date" id="fechaReserva" className="fechaReserva" min={handlerOnclick()}></input>
+        {/* Input de selección de hora */}
+        <input type="time" className="horaReserva" id="horaReserva" min="07:00" max="17:00" step="1800"></input>
+      </div>
+    );
+  }
 
 
 export function StateDetail() {
